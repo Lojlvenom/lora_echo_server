6d99193b3f3500be709b343f399db82a574cc7ba
joao camilo <jlf.camilo@icloud.com>
2025-03-18 11:03:55 -0400

    feat: add history and fix bugs.

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 from fastapi import FastAPI
 import uvicorn
+from pydantic import BaseModel
 app = FastAPI()
 
 sensor_data_db = []
 
+class SensorData(BaseModel):
+    data: dict
+
+
+@app.get("/sensors/history")
+def history():
+    return sensor_data_db
+
+
+@app.get("/sensors")
+def get_latest():
+    return sensor_data_db[-1]
+
 
 @app.post("/sensors")
-async def show_sensor_data(sensor_data):
-    sensor_data_db.append[sensor_data]
-    return sensor_data_db[-1]
+async def show_sensor_data(sensor_data: SensorData):
+    data = sensor_data.data
+    sensor_data_db.append(data)
+    return sensor_data.data
 
 
 if __name__ == '__main__':
